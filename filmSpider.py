30909959f213ac9f577aa81caff3ae837a512ffd
alessandro <alessandro.gianfelici@hotmail.com>
2021-09-24 22:17:08 +0200

    aggiunto filtro al crawler

--- filmSpider.py
+++ filmSpider.py
@@ -8,6 +8,19 @@ import os
 import io
 from bs4 import BeautifulSoup
 from datetime import datetime
+import pandas as pd
+
+path = os.path.dirname(__file__)
+def select_or_create(path):
+    try:
+        os.stat(path)
+    except FileNotFoundError:
+        os.mkdir(path)
+    return path
+
+savepath = select_or_create(os.path.join(os.path.dirname(__file__), "film"))
+film_data = pd.DataFrame()
+
 
 class MySpider(CrawlSpider):
     
@@ -26,10 +39,10 @@ class MySpider(CrawlSpider):
             mystring = ' '.join([par.get_text() for par in all_paragraph])
             text = " ".join(mystring.split())
             title = str(soup.find_all('title')[0]).replace(r'<title>', "").replace(" - MYmovies.it</title>", "")
-            txt_name = os.path.join("film", title+ '.txt')
-            s = io.StringIO(all_paragraph)
-            with open(txt_name, 'w') as f:
-                for line in s:
-                    f.write(line)
+            if 'Film' in title:
+                film_data['title'] = [title]
+                film_data['text'] = [text]
+                film_data['url'] = [response.url]
+                film_data.to_csv(os.path.join(savepath, title + '.csv'), index=0)
 
         self.log('crawling'.format(response.url))
